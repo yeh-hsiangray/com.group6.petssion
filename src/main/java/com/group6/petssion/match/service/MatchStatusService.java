0601b1888fc5375938b16f2e70fb88f9a3c37180
Elena <0723desire@gmail.com>
2021-06-23 17:47:00 +0800

    修改實體類別 及增加matchStatusController

--- src/main/java/com/group6/petssion/match/service/MatchStatusService.java
+++ src/main/java/com/group6/petssion/match/service/MatchStatusService.java
@@ -1,21 +1,8 @@
 package com.group6.petssion.match.service;
 
-import java.util.List;
-import java.util.Optional;
-
-import com.group6.petssion.bean.Hobby;
-import com.group6.petssion.bean.MatchStatus;
-
 public interface MatchStatusService {
 
-	Optional<Hobby> getHobbyById(int id);
-	
-	
-	List<Hobby> getHobby();
-	
-	//將配對狀態寫進資料庫
-	MatchStatus save(MatchStatus matchStatus);
-	
+
    
 	
 	
--- src/main/java/com/group6/petssion/match/service/impl/MatchStatusServiceImpl.java
+++ src/main/java/com/group6/petssion/match/service/impl/MatchStatusServiceImpl.java
@@ -1,18 +1,16 @@
 package com.group6.petssion.match.service.impl;
 
-import java.util.List;
-import java.util.Optional;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.group6.petssion.bean.Hobby;
-import com.group6.petssion.bean.MatchStatus;
 import com.group6.petssion.match.service.MatchStatusService;
 import com.group6.petssion.repository.HobbyRepository;
 import com.group6.petssion.repository.MatchStatusRepository;
 import com.group6.petssion.repository.UsersRepository;
 
+@Transactional
 @Service
 public class MatchStatusServiceImpl implements MatchStatusService {
 
@@ -21,31 +19,10 @@ public class MatchStatusServiceImpl implements MatchStatusService {
 
 	@Autowired
 	private HobbyRepository hobbyRepository;
-	
+
 	@Autowired
 	private MatchStatusRepository matchStatusRepository;
 
-	@Override
-	public Optional<Hobby> getHobbyById(int id) {
-		return hobbyRepository.findById(id);
-	}
 
-	@Override
-	public List<Hobby> getHobby() {
-		return hobbyRepository.findAll();
-	}
 
-	@Override
-	public MatchStatus save(MatchStatus matchStatus) {
-		return matchStatusRepository.save(matchStatus);
-	}
-
-	
-
-	
-	
-	
-	
-	
-	
 }
