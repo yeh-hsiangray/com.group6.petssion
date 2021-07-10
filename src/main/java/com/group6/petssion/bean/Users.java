eb8209b09cedcda78eb3e552a0d0404eefbeb694
Elena <0723desire@gmail.com>
2021-07-10 16:45:43 +0800

    User Bean 更改

--- src/main/java/com/group6/petssion/bean/Users.java
+++ src/main/java/com/group6/petssion/bean/Users.java
@@ -95,39 +95,6 @@ public class Users implements Serializable {
 
 	}
 
-	
-	
-
-	public Users(Integer id, String name, String gender, Date birthday, String address, String constellation,
-			Integer height, Integer weight, String mobilephone, String selfintroduction, String email, String garbled,
-			int checkemail, Integer manager, LocalDate regdate, Job job, Integer blockade, List<Hobby> hobby,
-			com.group6.petssion.bean.account_password account_password, List<Pet> pet, List<UsersImg> usersImg,
-			List<MultipartFile> img, String base64UserImg) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.gender = gender;
-		this.birthday = birthday;
-		this.address = address;
-		this.constellation = constellation;
-		this.height = height;
-		this.weight = weight;
-		this.mobilephone = mobilephone;
-		this.selfintroduction = selfintroduction;
-		this.email = email;
-		this.garbled = garbled;
-		this.checkemail = checkemail;
-		this.manager = manager;
-		this.regdate = regdate;
-		this.job = job;
-		Blockade = blockade;
-		this.hobby = hobby;
-		this.account_password = account_password;
-		this.pet = pet;
-		this.usersImg = usersImg;
-		this.img = img;
-		this.base64Img = base64Img;
-	}
 
 	public Users(Integer id, String name, String gender, Date birthday, String address, String constellation,
 			Integer height, Integer weight, String mobilephone, String selfintroduction, String email, Integer manager,
